90361894f12dcc69380a6ac0453982c47967f70b
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-08-31 17:02:18 +0300

    LifeCycle Hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,46 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, 
+    AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+  
   @Input('srvElement') element: {type:string, name:string, content:string,};
 
-  constructor() { }
+  constructor() {
+    console.log("Server-Component Constructor Called");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Server-Component ngOnChanges Called");
+    console.log(changes);
+  }
+
+  ngDoCheck(): void {
+    console.log("Server-Component ngDoCheck Called");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("Server-Component ngAfterContentInit Called");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("Server-Component ngAfterContentChecked Called");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("Server-Component ngAfterViewInit Called");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("Server-Component ngAfterViewChecked Called");
+  }
 
   ngOnInit(): void {
+    console.log("Server-Component ngOnInit Called");
+  }
+
+  ngOnDestroy(): void {
+    console.log("Server-Component ngOnDestroy Called");
   }
 
 }
